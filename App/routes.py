235a1d14e4dae2dd7d8fd57164b281cdca26ba10
Rakhmadiani Ardinda <rakhmadiani.chaerunnisa@gmail.com>
2024-12-23 22:25:09 +0700

    well

--- App/routes.py
+++ App/routes.py
@@ -179,7 +179,7 @@ def viewshow(queryShow):
                 akas_columns = [desc[0] for desc in cursor.description]
                 akas_dict = [dict(zip(akas_columns, row)) for row in akas]
 
-                cursor.execute('SELECT * FROM fix_titleepisode WHERE tconst = ?', (queryShow,))
+                cursor.execute('SELECT * FROM fix_titleepisode WHERE parentTconst = ?', (queryShow,))
                 episode = cursor.fetchall()
                 episode_columns = [desc[0] for desc in cursor.description]
                 episode_dict = [dict(zip(episode_columns, row)) for row in episode]
